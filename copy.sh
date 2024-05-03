743bd93d171f3d11cd1315a7e426210879d7d521
Puppy <112336978+PuppyAnimations@users.noreply.github.com>
2024-05-03 15:54:16 -0500

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -3,8 +3,8 @@
 
 clear
 
-wallpaper=$HOME/Pictures/wallpapers/Anime-Landscape2.png
-Waybar_Style="$HOME/.config/waybar/style/[Pywal] Chroma Tally.css"
+wallpaper=$HOME/Pictures/wallpapers/Lofi-Room.png
+Waybar_Style="$HOME/.config/waybar/style/[Transparent] Crystal Clear.css"
 
 # Check if running as root. If root, script will exit
 if [[ $EUID -eq 0 ]]; then
